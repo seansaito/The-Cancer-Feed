02902992ae157fcd090b9df45821e35185bbc890
seansaito <saitosean@ymail.com>
2015-02-27 09:32:49 +0800

    Added new learn page

--- app/views.py
+++ app/views.py
@@ -5,6 +5,35 @@ from config import ADMINS
 from scraper import get_mnr_articles, get_gov_articles, get_research_articles
 from random import randint
 
+#Global variable for storing list of educational websites
+websites = [
+    "http://www.cancer.gov/cancertopics/what-is-cancer",
+    "http://www.cancerresearchuk.org/about-cancer/type/breast-cancer/",
+    "http://www.cancerresearchuk.org/our-research/our-research-by-cancer-type/our-research-on-brain-tumours",
+    "http://www.cancerresearchuk.org/our-research/our-research-history",
+    "http://www.cancerresearchuk.org/cancer-info/spotcancerearly/",
+    "http://www.cancerresearchuk.org/cancer-info/cancerstats/keyfacts/",
+    "http://www.cancer.gov/cancertopics/types/leukemia",
+    "http://www.cancer.gov/cancertopics/types/lung",
+    "http://www.cancer.gov/cancertopics/types/pancreatic",
+    "http://www.cancer.gov/cancertopics/types/endometrial",
+    "http://www.cancer.gov/cancertopics/types/thyroid",
+    "http://www.cancer.gov/cancertopics/types/prostate",
+    "http://www.cancer.gov/cancertopics/types/bladder",
+    "http://www.cancer.net/navigating-cancer-care/how-cancer-treated/chemotherapy/what-chemotherapy",
+    "http://www.cancer.gov/cancertopics/treatment/types/radiation/radiation-fact-sheet",
+    "http://www.cancer.gov/cancertopics/treatment/types/surgery/lasers-fact-sheet",
+    "http://www.cancer.org/treatment/treatmentsandsideeffects/treatmenttypes/immunotherapy/immunotherapy-what-is-immunotherapy",
+    "http://www.cancer.org/research/infographicgallery/tobacco-related-healthcare-costs",
+    "http://www.cancer.gov/cancertopics/causes-prevention/risk-factors/tobacco/cessation-fact-sheet",
+    "http://www.cancerresearchuk.org/cancer-info/healthyliving/smoking-and-cancer/smoking-and-cancer",
+    "http://www.cancer.gov/cancertopics/factsheet/Sites-Types/metastatic",
+    "http://www.cancer.org/treatment/understandingyourdiagnosis/advancedcancer/advanced-cancer-what-is-metastatic",
+    "http://www.biooncology.com/biological-pathways/angiogenic-signaling",
+    "http://www.ncbi.nlm.nih.gov/pmc/articles/PMC1993983/",
+    "http://www.cancer.gov/cancertopics/treatment/types/biological-therapies/angiogenesis-inhibitors-fact-sheet"
+]
+
 @application.route("/", methods = ["GET", "POST"])
 @application.route("/index", methods=["GET", "POST"])
 def index():
@@ -63,6 +92,12 @@ def feedback_answer():
     if q6 == "42":
         return redirect(url_for("prize"))
     return redirect(url_for("index"))
+
+@application.route("/learn")
+def learn():
+    page = randint(0, len(websites) - 1)
+    random_website = websites[page]
+    return redirect(random_website)
 
 @application.route("/prize")
 def prize():
